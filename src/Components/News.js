539fd5de654400b0f0351ec21d4bb8faa131c58e
Ahmad Piracha <ahmadpiracha3@gmail.com>
2024-10-09 21:41:44 +0500

    final

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,6 +3,8 @@ import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
+const key = process.env.API_KEY;
+console.log(key);
 
 const News = (props) => {
   // REACT HOOKS
@@ -10,11 +12,15 @@ const News = (props) => {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
-  const capitalizeFirstLetter = (string) => {return string.charAt(0).toUpperCase() + string.slice(1);};
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   const updateData = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=a86dedc73a3c451b896dbf7e596cdbd9&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${key}&page=${page}&pageSize=${props.pageSize}`;
+    console.log(url);
+    
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
@@ -26,14 +32,21 @@ const News = (props) => {
     setPage(page + 1);
     props.setProgress(100);
   };
-  useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - ZipperNews`
-    updateData();
-    //esline-disable-next-line
-  }, []);
+  useEffect(
+    () => {
+      document.title = `${capitalizeFirstLetter(props.category)} - ZipperNews`;
+      updateData();
+    },
+    // eslint-disable-next-line
+    []
+  );
 
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=a86dedc73a3c451b896dbf7e596cdbd9&page=${page+1}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apikey=${key}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
     setPage(page + 1);
     let data = await fetch(url);
     let parseData = await data.json();
@@ -42,8 +55,14 @@ const News = (props) => {
   };
 
   return (
-    <div className="container my-3" >
-      <h1 className="text-center" style={{marginTop:'90px',color: props.mode === "light" ? "#2F4F4F" : "white"}}>
+    <div className="container my-3">
+      <h1
+        className="text-center"
+        style={{
+          marginTop: "90px",
+          color: props.mode === "light" ? "#2F4F4F" : "white",
+        }}
+      >
         ZipperNews - Top {capitalizeFirstLetter(props.category)} Headlines
       </h1>
       {loading && <Spinner />}
@@ -75,7 +94,6 @@ const News = (props) => {
           </div>
         </div>
       </InfiniteScroll>
-     
     </div>
   );
 };
@@ -93,60 +111,59 @@ News.propTypes = {
 };
 export default News;
 
-
 // Next and Previous Button code SAMPLE:
 
-  // <div className="container d-flex justify-content-between">
-  //           <button
-  //             disabled={page <= 1}
-  //             className="btn btn-dark mx-2"
-  //             onClick={this.handlePrevClick}
-  //           >
-  //             &larr; Previous
-  //           </button>
-  //           <button
-  //             disabled={
-  //               page + 1 >
-  //               Math.ceil(totalResults / props.pageSize)
-  //             }
-  //             className="btn btn-dark mx-2"
-  //             onClick={this.handleNextClick}
-  //           >
-  //             Next &rarr;
-  //           </button>
-  //         </div> 
+// <div className="container d-flex justify-content-between">
+//           <button
+//             disabled={page <= 1}
+//             className="btn btn-dark mx-2"
+//             onClick={this.handlePrevClick}
+//           >
+//             &larr; Previous
+//           </button>
+//           <button
+//             disabled={
+//               page + 1 >
+//               Math.ceil(totalResults / props.pageSize)
+//             }
+//             className="btn btn-dark mx-2"
+//             onClick={this.handleNextClick}
+//           >
+//             Next &rarr;
+//           </button>
+//         </div>
 
-  // handlePrevClick = async () => {
-  //   // console.log("Previous");
-  //   // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=${props.pageSize}`;
-  //   // this.setState({ loading: true });
+// handlePrevClick = async () => {
+//   // console.log("Previous");
+//   // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=${props.pageSize}`;
+//   // this.setState({ loading: true });
 
-  //   // let data = await fetch(url);
-  //   // let parsedData = await data.json();
-  //   // console.log(parsedData);
-  //   // this.setState({
-  //   //   page: this.state.page - 1,
-  //   //   articles: parsedData.articles,
-  //   //   loading: false,
-  //   // });
-  //   this.setState({ page: this.state.page - 1 });
-  //   updateData();
-  // };
-  // handleNextClick = async () => {
-  //   // console.log("Next");
-  //   // if (this.state.page + 1 > Math.ceil(this.state.totalResults / props.pageSize)) {
-  //   // }
-  //   // else {
-  //   //   let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
-  //   //   this.setState({ loading: true });
-  //   //   let data = await fetch(url);
-  //   //   let parsedData = await data.json();
-  //   //   this.setState({
-  //   //     page: this.state.page + 1,
-  //   //     articles: parsedData.articles,
-  //   //     loading: false,
-  //   //   });
-  //   // }
-  //   this.setState({ page: this.state.page + 1 });
-  //   updateData();
-  // };
+//   // let data = await fetch(url);
+//   // let parsedData = await data.json();
+//   // console.log(parsedData);
+//   // this.setState({
+//   //   page: this.state.page - 1,
+//   //   articles: parsedData.articles,
+//   //   loading: false,
+//   // });
+//   this.setState({ page: this.state.page - 1 });
+//   updateData();
+// };
+// handleNextClick = async () => {
+//   // console.log("Next");
+//   // if (this.state.page + 1 > Math.ceil(this.state.totalResults / props.pageSize)) {
+//   // }
+//   // else {
+//   //   let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
+//   //   this.setState({ loading: true });
+//   //   let data = await fetch(url);
+//   //   let parsedData = await data.json();
+//   //   this.setState({
+//   //     page: this.state.page + 1,
+//   //     articles: parsedData.articles,
+//   //     loading: false,
+//   //   });
+//   // }
+//   this.setState({ page: this.state.page + 1 });
+//   updateData();
+// };
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -8,7 +8,7 @@ const NewsItem = (props) => {
                 <img src={!imageUrl ? "https://image.cnbcfm.com/api/v1/image/107085478-1657185428155-gettyimages-1405436430-dscf9132_lqjmzb4m.jpeg?v=1657185479&w=1920&h=1080" : imageUrl} className="card-img-top" alt=" Img" />
                 <div className="card-body">
                     <h5 className="card-title">{title}
-                        <span class="badge rounded-pill bg-dark mx-3">By {source}</span></h5>
+                        <span className="badge rounded-pill bg-dark mx-3">By {source}</span></h5>
                     <p className="card-text">{description}</p>
                     <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
                     <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More...</a>
